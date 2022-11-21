a684f49e821159489b1db8109d02a381b8280c7d
FelipePaulino <felipepaulino08@hotmail.com>
2022-11-21 13:32:13 -0300

    backup

--- pokemon/pages/index.js
+++ pokemon/pages/index.js
@@ -1,5 +1,18 @@
+import styles from '../styles/Home.module.css'
+import { Router } from 'next/router'
+
 export default function Home() {
   return (
-   <h1>hello world</h1>
+    <div className={styles.container}>
+      <form className={styles.formsContainer}>
+        <div>
+          <input type={"text"} name={"name"} placeholder={"POKEMON"} className={styles.formRequest} />
+          <button type='submit' className={styles.formRequest}>Abrir</button>
+        </div>
+        <div>
+          <button type='button' className={styles.button} onClick={() => {Router.push('/Characters')}}>All Pokemons</button>
+        </div>
+      </form>
+    </div>
   )
 }
