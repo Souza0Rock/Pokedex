a9a46c7d59c68e91fabcd38784770eb368160a0f
FelipePaulino <felipepaulino08@hotmail.com>
2022-11-22 11:25:12 -0300

    backup

--- pokemon/components/Main/index.js
+++ pokemon/components/Main/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Modal from "react-modal";
 import styles from '../../styles/Characters.module.css'
+import modalStyle from '../../styles/ModalStyles.module.css'
+
+Modal.setAppElement("#__next");
 
 export default function Main () {
 
@@ -13,19 +17,47 @@ export default function Main () {
         setCharacter(response.data.results))
     }, [])
 
-    console.log(character)
+    const [modalVisible, setModalVisible] = useState(false);
+    const [itemSelect, setItemSelect] = useState(null);
+
+    const onItemClicked = (item) => {
+        setItemSelect(item);
+        setModalVisible(true);
+    }
+
+    function closeModal() {
+        setModalVisible(false);
+    }
 
     return (
         <main>
             <ul className={styles.ulCharacters}>
-            {character?.map((character, index) => {
-                return (
-                    <li key={index} className={styles.li}>
-                        <h3 className={styles.li_name}>{character?.name}</h3>
-                    </li>
+                {character?.map((character, index) => {
+                    return (
+                        <li key={index} className={styles.li} onClick={() => onItemClicked(character)}>
+                            <h3 className={styles.li_name}>{character?.name}</h3>
+                        </li>
                     );
                 })}
             </ul>
+            <div className={modalStyle.container}>
+                <Modal
+                isOpen={modalVisible}
+                onRequestClose={closeModal}
+                overlayClassName={modalStyle.modalOverlay}
+                className={modalStyle.modalContent}>
+                    <div>
+                        <h1>{itemSelect?.name}</h1>
+                    </div>
+                    <ul>
+                        {character?.map((character, index) => {
+                            return (
+                                
+                            )
+                        })}
+                    </ul>
+                </Modal>
+            </div>
         </main>
     )
 }
