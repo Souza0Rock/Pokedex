23890d94ab6b46492f76648b2fa337dc2e1b903b
FelipePaulino <felipepaulino08@hotmail.com>
2022-11-25 18:51:39 -0300

    search bar correction

--- pokemon/components/SectionHome/index.js
+++ pokemon/components/SectionHome/index.js
@@ -11,10 +11,14 @@ export default function SectionHome() {
     const [pokemon, setPokemon] = useState("");
     const [openModal, setOpenModal] = useState(false);
 
-        const pokemoLendas = (choice) => {
-            axios
-            .get(`https://pokeapi.co/api/v2/pokemon/${choice}`)
+    const pokemoLendas = () => {
+        console.log('oi')
+
+        console.log(pokemon, 'choice')
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
             .then((response) => {
+                console.log(response, 'response')
                 setResposta(response.data);
                 setOpenModal(true);
                 console.log('aqui');
@@ -22,14 +26,7 @@ export default function SectionHome() {
             .catch((error) => {
                 alert("Quem é esse pokemon?");
             });
-        };
-        
-        console.log(resposta, 'blablabla')
-        // useEffect (() => {
-        //     pokemoLendas()
-        // }, [])
-
-        
+    };
 
     function closeModal() {
         setOpenModal(false);
@@ -38,58 +35,57 @@ export default function SectionHome() {
     return (
         <section>
             <div className={styles.container}>
-                <form className={styles.formsContainer}>
                     <div>
-                        <input type={"text"} placeholder={"Encontre seu pokemon!"} className={styles.formRequest} onChange= {(e) => setPokemon(e.target.value)} />
-                        <button type='submit' className={styles.formRequest} onClick= {() => pokemoLendas(pokemon)}>Abrir</button>
+                        <input type={"text"} placeholder={"Encontre seu pokemon!"} className={styles.formRequest} onChange={(e) => setPokemon(e.target.value)} />
+                        <button type='submit' className={styles.formRequest} onClick={() => pokemoLendas()}>Abrir</button>
                     </div>
                     <div>
-                        <button type='button' className={styles.button} onClick={() => {Router.push('/Characters')}}>Todos os Pokemons</button>
+                        <button type='button' className={styles.button} onClick={() => { Router.push('/Characters') }}>Todos os Pokemons</button>
                     </div>
-                </form>
+            
                 {openModal && resposta && (
-                <Modal
-                isOpen={openModal}
-                onRequestClose={closeModal}
-                overlayClassName={modalStyle.modalOverlay}
-                className={modalStyle.modalContent}>
-                    <section className={modalStyle.section}>
-                        <div className={modalStyle.nameDiv}>
-                            <h1 className={modalStyle.title}>{resposta && resposta?.name}</h1>
-                        </div>
-                        <div className={modalStyle.contentTxt}>
-                            <ul>                    
-                                <li className={modalStyle.li_info}>
-                                    <p className={modalStyle.li_txt}>hp = {resposta && resposta?.stats[0]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>
-                                    <p className={modalStyle.li_txt}>attack = {resposta && resposta?.stats[1]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>                   
-                                    <p className={modalStyle.li_txt}>defense = {resposta && resposta?.stats[2]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>                   
-                                    <p className={modalStyle.li_txt}>special-attack = {resposta && resposta?.stats[3]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>                   
-                                    <p className={modalStyle.li_txt}>special-defense = {resposta && resposta?.stats[4]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>                   
-                                    <p className={modalStyle.li_txt}>speed = {resposta && resposta?.stats[5]?.base_stat}</p>
-                                </li>
-                                <li className={modalStyle.li_info}>                   
-                                    <p className={modalStyle.li_txt}>type = {resposta && resposta?.types[0]?.type?.name}</p>
-                                </li>                       
-                            </ul>
-                        </div>
-                        <div className={modalStyle.divModalImg}>
-                            <img src={resposta && resposta?.sprites?.front_default} alt={pokemon?.name} className={modalStyle.modalImg}/>
-                        </div>
-                        <div className={modalStyle.divXClose} onClick={() => setOpenModal(false)}>
-                            <p className={modalStyle.xClose}>x</p>
-                        </div>
-                    </section>
-                </Modal>)}
+                    <Modal
+                        isOpen={openModal}
+                        onRequestClose={closeModal}
+                        overlayClassName={modalStyle.modalOverlay}
+                        className={modalStyle.modalContent}>
+                        <section className={modalStyle.section}>
+                            <div className={modalStyle.nameDiv}>
+                                <h1 className={modalStyle.title}>{resposta && resposta?.name}</h1>
+                            </div>
+                            <div className={modalStyle.contentTxt}>
+                                <ul>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>hp = {resposta && resposta?.stats[0]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>attack = {resposta && resposta?.stats[1]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>defense = {resposta && resposta?.stats[2]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>special-attack = {resposta && resposta?.stats[3]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>special-defense = {resposta && resposta?.stats[4]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>speed = {resposta && resposta?.stats[5]?.base_stat}</p>
+                                    </li>
+                                    <li className={modalStyle.li_info}>
+                                        <p className={modalStyle.li_txt}>type = {resposta && resposta?.types[0]?.type?.name}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={modalStyle.divModalImg}>
+                                <img src={resposta && resposta?.sprites?.front_default} alt={pokemon?.name} className={modalStyle.modalImg} />
+                            </div>
+                            <div className={modalStyle.divXClose} onClick={() => setOpenModal(false)}>
+                                <p className={modalStyle.xClose}>x</p>
+                            </div>
+                        </section>
+                    </Modal>)}
             </div>
         </section>
     )
