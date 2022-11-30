c0d2535c85e75421add60a64aa625233ef1816cf
FelipePaulino <felipepaulino08@hotmail.com>
2022-11-30 14:28:32 -0300

    backup

--- pokemon/components/Header/index.js
+++ pokemon/components/Header/index.js
@@ -3,15 +3,35 @@ import Router from 'next/router'
 import header from '../../styles/Header.module.css'
 
 export default function Header () {
-    
+
+    const [busca, setBusca] = useState('');
+
+    const frutas = [
+        'banana',
+        'maça',
+        'laranja',
+        'abacaxi',
+        'abacate',
+        'uva'
+    ]
+
+    const frutasFiltradas = frutas
+        .filter((fruta) => fruta.startsWith(busca));
+
+    console.log(frutasFiltradas, 'busca');
+
     return (
         <header className={header.header}>
             <div className={header.container}>
                 <img alt='voltar' src='../images/icon-voltar.png' className={header.iconBack} onClick={() => Router.push('../')} />                    
                 <h1 className={header.header_title}>Lista de Pokemons</h1>
-                <form className={header.formsContainer}>
-                    <input type={"text"} placeholder={"Encontre seu pokemon"} className={header.formRequest} />
-                </form>
+                <div className={header.formsContainer}>
+                    <input type={"text"} 
+                    placeholder={"Encontre seu pokemon"} 
+                    value={busca}
+                    onChange={(ev) => setBusca(ev.target.value)}
+                    className={header.formRequest} />
+                </div>
             </div>
         </header>        
     )
